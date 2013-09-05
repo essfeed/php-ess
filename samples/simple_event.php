29ab39c84240520273d6393cb9ea823d90300c18
Brice Pissard <brice@hypecal.com>
2013-09-05 16:51:49 +0200

    Update Sample description

--- samples/simple_event.php
+++ samples/simple_event.php
@@ -25,13 +25,14 @@
 	$event_webpage = 'http://madonna.com/events/concert.html';
 	
 	// Create an Event 
-	$newEvent = $essFeed->newEventFeed( array( 'title'=> 'Madonna Concert', 'uri'=> $event_webpage, 'published'=> 'now', 'access'=> 'PUBLIC', 'description' => "This is the description of the Madonna concert. Plus some HTML: <br><br><img src='http://madonna.com/img/concert.png' alt='The text of the image comming from the description!'>",'tags'=> array( 'music', 'pop', '80s', 'Madonna', 'concert' )));
+	$newEvent = $essFeed->newEventFeed( array( 'title'=> 'Madonna Concert', 'uri'=> $event_webpage, 'published'=> 'now', 'access'=> 'PUBLIC', 'description' => "This is the description of the Madonna concert. Plus some HTML: <br><br><img src='http://3432-stoollala.voxcdn.com/wp-content/uploads/2010/09/madonna_old.jpg' alt='The text of the image comming from the description!'>",'tags'=> array( 'music', 'pop', '80s', 'Madonna', 'concert' )));
   		$newEvent->addCategory( 'concert', 											array('name'=> 'Rock Music', 'id'=> 'M22'));
 		$newEvent->addDate( 	'recurrent', 'month', 6, 1, 'saturday', 'last',		array('name'=> 'Every last saturday of every months for 6 months', 'start'=> 1316898400, 'duration'=> '7200' ) );
-		$newEvent->addPlace( 	'fixed', null,										array('name'=> 'Yankee Stadium', 'address' => '1 E 161st St, Bronx', 'city' => 'New York', 'zip' => '10451', 'state' => 'New York', 'state_code' => 'NY', 'country' => 'United States of America', 'country_code' => 'US' ) );
+		$newEvent->addPlace( 	'fixed', null,										array('name'=> 'Yankee Stadium', 'address' => '1 East, 161street, Bronx', 'city' => 'New York', 'zip' => '10451', 'state' => 'New York', 'state_code' => 'NY', 'country' => 'United States of America', 'country_code' => 'US' ) );
 		$newEvent->addPrice(	'standalone', 'fixed', null,null,null,null,null,	array('name'=> 'Entrance with VIP access', 'value'=> '90', 'currency'=> 'USD', 'uri'=> 'http://madonna.com/payment/api'));
 		$newEvent->addPeople(	'performer',										array('name'=> 'Madonna' ) );
-		$newEvent->addMedia(	'image', 											array('name'=> 'Foto of Madonna', 'uri' => 'http://madonna.com/image.png'));					
+		$newEvent->addMedia(	'image', 											array('name'=> 'Foto of Madonna', 'uri' => 'http://www.innatfalsecreek.com/controls/FckEditor/editor/filemanager/userfiles/show_tickets_madonna_4963_png.jpg'));					
+		$newEvent->addMedia(	'image', 											array('name'=> 'Another image', 'uri' => 'http://1.bp.blogspot.com/-7EQAFopc6lk/UVX_ofAUQeI/AAAAAAAACWs/Le2dvu5nlFM/s640/dona.jpg'));					
 		// --------------------------------------------------------------------------------------------------------
 		//	YOU CAN ALSO:
 		// 	Parse the description to extract media files URL from an HTML content. (image, sound, youtube...)
@@ -50,6 +51,6 @@
 	//$essFeed->genarateFeed( $feed_on_server );
 	
 	// OR Generate the ESS XML feed dynamicaly through PHP (can load PHP and the DataBase if a lot of robots access to this dynamic file).
-	$essFeed->genarateFeed();
-	//$essFeed->genarateFeed( '/home/bibi/www/ess.hypecal.com/php-ess/samples/simple_event.ess' );
+	//$essFeed->genarateFeed();
+	$essFeed->genarateFeed( '/home/bibi/www/ess.hypecal.com/php-ess/samples/simple_event.ess' );
 	
